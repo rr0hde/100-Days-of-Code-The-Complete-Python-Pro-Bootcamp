07861c5397d7330b9949073d871377c9ea1d175e
rr0hde <115171979+rr0hde@users.noreply.github.com>
2022-10-17 16:35:48 -0700

    day030

--- day030/main.py
+++ day030/main.py
@@ -5,6 +5,24 @@ from pyperclip import copy
 import json
 
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exist.")
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def password_gen():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -42,12 +60,21 @@ def save():
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        with open("data.json", mode="w") as data_file:
-            json.dump(new_data, data_file, indent=4)
+        try:
+            with open("data.json", mode="r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as data_file:
+                data.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
 
-        website_entry.delete(0, END)
-        pass_entry.delete(0, END)
-        website_entry.focus()
+            with open("data.json", mode="w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            pass_entry.delete(0, END)
+            website_entry.focus()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -63,8 +90,8 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1, sticky=E)
 
-website_entry = Entry(width=52)
-website_entry.grid(column=1, row=1, columnspan=2, sticky=W)
+website_entry = Entry(width=33)
+website_entry.grid(column=1, row=1, sticky=W)
 website_entry.focus()
 
 email_label = Label(text="Email/Username:")
@@ -85,5 +112,8 @@ gen_button.grid(column=2, row=3, sticky=W)
 
 add_button = Button(text="Add", width=44, command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky=W)
+
+search_button = Button(text="Search", width=14, command=find_password)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
