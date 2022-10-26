38781d4e3be9ecd7dece0792147ce16e2c5d3b7a
rr0hde <115171979+rr0hde@users.noreply.github.com>
2022-10-25 17:14:31 -0700

    day040

--- day039/main.py
+++ day039/main.py
@@ -1,16 +1,35 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
+from datetime import datetime, timedelta
 
 data_manager = DataManager()
 flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
 sheet_data = data_manager.get_destination_data()
 
 if sheet_data[0]["iataCode"] == "":
-    for row in sheet_data:
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
+    city_names = [row["city"] for row in sheet_data]
+    print(city_names)
+    codes = flight_search.get_destination_codes(city_names)
+    data_manager.update_destination_codes(codes)
+    sheet_data = data_manager.get_destination_data()
 
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+today = datetime.now() + timedelta(1)
+six_month_from_today = datetime.now() + timedelta(6 * 30)
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=today,
+        to_time=six_month_from_today
+    )
+
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to"
+                    f" {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to "
+                    f"{flight.return_date}."
+        )
--- day039/notification_manager.py
+++ day039/notification_manager.py
@@ -1,3 +1,21 @@
+from twilio.rest import Client
+
+TWILIO_SID = "ACf772fa6713b8985ef23c5b838c8bdae3"
+TWILIO_AUTH_TOKEN = "5f624f631e4b92a2bdca8894957f8e67"
+TWILIO_VIRTUAL_NUMBER = "+18142613653"
+TWILIO_VERIFIED_NUMBER = "+14805282664"
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
